9487c114223d60dfc2c41f5067278880cfb38f14
Ankush Gupta <ankushgupta9675@gmail.com>
2023-07-14 12:37:53 +0530

    solve callback error

--- Controller/Admin/authController.js
+++ Controller/Admin/authController.js
@@ -10,72 +10,72 @@ const { Op } = require("sequelize");
 
 const SALT = 10;
 
-// exports.registerAdmin = async (req, res) => {
-//     try {
-//         const { error } = adminRegistration(req.body);
-//         if (error) {
-//             return res.status(400).send(error.details[0].message);
-//         }
-//         const isAdmin = await AdminInformation.findOne({
-//             where: {
-//                 email: req.body.email,
-//             },
-//         });
-//         if (isAdmin) {
-//             return res.status(400).send({
-//                 success: false,
-//                 message: "Admin already present!"
-//             });
-//         }
-//         // generate employee code
-//         let code;
-//         const isAdminCode = await AdminInformation.findAll({
-//             paranoid: false,
-//             order: [
-//                 ['createdAt', 'ASC']
-//             ],
-//             // attributes: [
-//             //     [Sequelize.fn('DISTINCT', Sequelize.col('employeesCode')) ,'employeesCode']
-//             // ]
-//         });
-//         if (isAdminCode.length == 0) {
-//             code = "ADMIN" + 1000;
-//         } else {
-//             let lastAdminCode = isAdminCode[isAdminCode.length - 1];
-//             let lastDigits = lastAdminCode.adminCode.substring(5);
-//             let incrementedDigits = parseInt(lastDigits, 10) + 1;
-//             code = "ADMIN" + incrementedDigits;
-//             //  console.log(code);
-//         }
-//         const salt = await bcrypt.genSalt(SALT);
-//         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-//         const admin = await AdminInformation.create({
-//             ...req.body,
-//             password: hashedPassword,
-//             adminCode: code
-//         });
-//         const data = {
-//             id: admin.id,
-//             email: req.body.email,
-//             code: admin.adminCode
-//         }
-//         const authToken = jwt.sign(
-//             data,
-//             JWT_SECRET_KEY,
-//             { expiresIn: JWT_VALIDITY } // five day
-//         );
-//         res.status(200).send({
-//             success: true,
-//             message: 'Register successfully!',
-//             authToken: authToken
-//         });
-//     } catch (err) {
-//         res.status(500).send({
-//             success: false,
-//             message: err.message
-//         });
-//     }
-// };
+exports.registerAdmin = async (req, res) => {
+    try {
+        const { error } = adminRegistration(req.body);
+        if (error) {
+            return res.status(400).send(error.details[0].message);
+        }
+        const isAdmin = await AdminInformation.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (isAdmin) {
+            return res.status(400).send({
+                success: false,
+                message: "Admin already present!"
+            });
+        }
+        // generate employee code
+        let code;
+        const isAdminCode = await AdminInformation.findAll({
+            paranoid: false,
+            order: [
+                ['createdAt', 'ASC']
+            ],
+            // attributes: [
+            //     [Sequelize.fn('DISTINCT', Sequelize.col('employeesCode')) ,'employeesCode']
+            // ]
+        });
+        if (isAdminCode.length == 0) {
+            code = "ADMIN" + 1000;
+        } else {
+            let lastAdminCode = isAdminCode[isAdminCode.length - 1];
+            let lastDigits = lastAdminCode.adminCode.substring(5);
+            let incrementedDigits = parseInt(lastDigits, 10) + 1;
+            code = "ADMIN" + incrementedDigits;
+            //  console.log(code);
+        }
+        const salt = await bcrypt.genSalt(SALT);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const admin = await AdminInformation.create({
+            ...req.body,
+            password: hashedPassword,
+            adminCode: code
+        });
+        const data = {
+            id: admin.id,
+            email: req.body.email,
+            code: admin.adminCode
+        }
+        const authToken = jwt.sign(
+            data,
+            JWT_SECRET_KEY,
+            { expiresIn: JWT_VALIDITY } // five day
+        );
+        res.status(200).send({
+            success: true,
+            message: 'Register successfully!',
+            authToken: authToken
+        });
+    } catch (err) {
+        res.status(500).send({
+            success: false,
+            message: err.message
+        });
+    }
+};
 
 exports.loginAdmin = async (req, res) => {
     try {
